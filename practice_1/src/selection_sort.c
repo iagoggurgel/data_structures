2c689ecc5b9d7d3474c53de3703d938a521c538f
Iago Gouveia Gurgel <iagoggurgel25@gmail.com>
2024-07-14 01:18:02 -0300

    feat: implementing selection sort

---
+++ practice_1/src/selection_sort.c
@@ -0,0 +1,31 @@
+#include <alg.h>
+
+
+
+/*
+    Algoritmo de ordenação Selection Sort
+
+    Entradas:
+        v: ponteiro para um vetor de inteiros
+        n: número de elementos do vetor v
+
+*/
+void sSort(int * v, int n)
+{
+    int min;
+    for (int i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for (int j = i + 1; i < n; i++)
+        {
+            if (v[j] < v[min])
+            {
+                min = j;
+            }
+        }
+        if (i != min)
+        {
+            swap(v[i], v[min]);
+        }
+    }
+}
