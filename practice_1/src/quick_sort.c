ee640179ccd47c221aae43eb8597a0bac941c518
Iago Gouveia Gurgel <iagoggurgel25@gmail.com>
2024-07-14 01:18:28 -0300

    feat: implementing quick sort

---
+++ practice_1/src/quick_sort.c
@@ -0,0 +1,50 @@
+#include <alg.h>
+
+
+/*
+    Algoritmo de ordenação Quick Sort
+
+    Entradas:
+        v: vetor de inteiros
+        s: primeiro elemento
+        e: último elemento
+
+*/
+void qSort(int * v, int s, int e)
+{
+    if (s >= 0 && e >= 0 && s < e)
+    {
+        int p = partition(v, s, e);
+        qSort(v, s, p);
+        qSort(v, p + 1, e);
+    }
+}
+
+int partition(int * v, int s, int e)
+{
+
+    int pivot = v[s];
+
+    int i = s - 1;
+    int j = e + 1;
+
+    while (1)
+    {
+        do
+        {
+            i += 1;
+        } while (v[i] < pivot);
+
+        do
+        {
+            j -= 1;
+        } while (v[j] > pivot);
+        
+        if (i >= j)
+        {
+            return j;
+        }
+
+        swap(v[i], v[j]);
+    }
+}
