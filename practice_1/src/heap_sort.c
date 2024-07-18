2ee192dfd742e639cf95237ea2a52c79afd0d24b
Iago Gouveia Gurgel <iagoggurgel25@gmail.com>
2024-07-18 04:37:11 -0300

    FEAT: heap sort ready

--- practice_1/src/heap_sort.c
+++ practice_1/src/heap_sort.c
@@ -1,14 +1,66 @@
 #include "alg.h"
 
-struct node_h
-{
-    int data;
-    struct node_h * left;
-    struct node_h * right;
-};
+void heapify(int *, int, int);
 
 
-void hSort(int *, int n)
+/*
+    Algoritmo de ordenação Heap Sort
+
+    Entradas:
+        v: vetor de inteiros
+        n: número de elementos de v
+*/
+void hSort(int * v, int n)
 {
-    
+    int i;
+
+    for (i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(v, n, i);
+    }
+
+    // Heap sort
+    for (int i = n - 1; i > 0; i--) {
+
+        swap(&v[0], &v[i]);
+
+        heapify(v, i, 0);
+    }
+}
+
+
+/*
+    Função auxiliar para realizar o Heapify no vetor v.
+
+    Entradas:
+        v: vetor de inteiros
+        n: tamanho da heap
+        i: "nó" da heap (índice do vetor v)
+*/
+void heapify(int * v, int n, int i)
+{
+
+
+    int largest = i;
+
+    int left = 2 * i + 1;
+
+    int right = 2 * i + 2;
+
+    if (left < n && v[left] > v[largest])
+    {
+        largest = left;
+    }
+
+    if (right < n && v[right] > v[largest])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(&v[i], &v[largest]);
+
+        heapify(v, n, largest);
+    }
 }
