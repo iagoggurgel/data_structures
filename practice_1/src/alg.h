5d83b084b1320371583cae88143470d45ca413fb
Iago Gouveia Gurgel <iagoggurgel25@gmail.com>
2024-07-18 04:09:41 -0300

    CHORE: adding new algorithms

--- practice_1/src/alg.h
+++ practice_1/src/alg.h
@@ -8,12 +8,16 @@
 void iSort(int *, int);
 void sSort(int *, int);
 void qSort(int *, int, int);
-void mSort(int *, int);
+void mSort(int *, int, int);
 void dSort(int *, int, int);
+void cSort(int * v, int);
+void hSort(int *, int);
+void quineMcCluskey();
 
 void swap(int *, int *);
 void printArray(int *, int);
 int isSorted(int *, int);
 int max(int *, int);
+int min(int *, int);
 
 #endif
